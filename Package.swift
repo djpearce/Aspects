2feadda31ae764a74142f982ad053696666f593b
Dylan Pearce <djpearce13@gmail.com>
2021-12-01 17:39:46 -0500

    include as header path

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Aspects",
-            type: .dynamic,
             targets: ["Aspects"]),
     ],
     dependencies: [
@@ -22,7 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Aspects",
-            dependencies: [],
-            publicHeadersPath: "Public"),
+            dependencies: []),
     ]
 )
